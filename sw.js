021a170621293c16d2bf5e515e5080fcc52ceb0b
ramrambhai09 <ramahir439@gmail.com>
2026-06-07 10:48:05 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,4 +5,4 @@ self.options = {
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
 
-<script src="https://5gvci.com/act/files/tag.min.js?z=11112421" data-cfasync="false" async></script>
+
